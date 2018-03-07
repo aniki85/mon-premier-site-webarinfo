33a6b26b043668c8d1b0f0a860448dc0be8d9104
Unknown <gaetan.delchateau@orange.fr>
2018-03-07 10:27:18 +0100

    icone

---
+++ ressource/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("h1").click(function () {
+        $(this).css("background-color", "#ff0000");
+    })
+});
+$(document).ready(function() {
+    $("h2").click(function () {
+        $(this).css("border", "1px solid red");
+    })
+});
